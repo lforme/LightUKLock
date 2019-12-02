cefff893d23814dd4e6d150b718f30e048b7b1cf
木瓜 <lforme@163.com>
2019-12-02 21:37:59 +0800

    消息中心页面

--- LightSmartLock/Controllers/Lock/MessageCenterController.swift
+++ LightSmartLock/Controllers/Lock/MessageCenterController.swift
@@ -7,8 +7,15 @@
 //
 
 import UIKit
+import PKHUD
+import RxCocoa
+import ReactorKit
+import RxSwift
+import MJRefresh
+import SwiftDate
+import Action
 
-fileprivate class MessageCenterCell: UITableViewCell {
+class MessageCenterCell: UITableViewCell {
     
     @IBOutlet weak var titleLabel: UILabel!
     @IBOutlet weak var timeLabel: UILabel!
@@ -17,10 +24,26 @@ fileprivate class MessageCenterCell: UITableViewCell {
     @IBOutlet weak var ignoreButton: UIButton!
     @IBOutlet weak var agreeButton: UIButton!
     
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+    }
+    
     override func awakeFromNib() {
         super.awakeFromNib()
         
         setupUI()
+    }
+    
+    func bind(_ data: CenterMessageModel) {
+        titleLabel.text = data.title
+        content.text = data.content
+        if let time = data.createDate?.toDate()?.toString(.custom("MM / dd  HH:mm")) {
+            timeLabel.text = time
+        }
     }
     
     func setupUI() {
@@ -33,17 +56,38 @@ fileprivate class MessageCenterCell: UITableViewCell {
         // 这期不做
         buttonContainer.removeArrangedSubview(ignoreButton)
         buttonContainer.removeArrangedSubview(agreeButton)
+        ignoreButton.removeFromSuperview()
+        agreeButton.removeFromSuperview()
+        layoutIfNeeded()
     }
 }
 
-class MessageCenterController: UITableViewController {
-
+class MessageCenterController: UITableViewController, StoryboardView {
+    
+    typealias Reactor = MessageCenterReactor
+    
+    var disposeBag: DisposeBag = DisposeBag()
+    var dataSource: [CenterMessageModel] = []
+    
+    private var filterButton: UIButton!
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        self.tableView.mj_header?.beginRefreshing()
+    }
+    
+    deinit {
+        print("\(self) deinit")
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         title = "消息中心"
         setupUI()
         setupRightNavigationItem()
+        
+        self.reactor = Reactor()
     }
     
     func setupUI() {
@@ -52,6 +96,67 @@ class MessageCenterController: UITableViewController {
     }
     
     func setupRightNavigationItem() {
-        createdRightNavigationItem(title: nil, image: UIImage(named: "message_filter"))
+        self.filterButton = createdRightNavigationItem(title: nil, image: UIImage(named: "message_filter"))
+    }
+    
+    func bind(reactor: MessageCenterReactor) {
+        
+        self.tableView.mj_header = MJRefreshNormalHeader(refreshingBlock: {[weak self] in
+            guard let this = self else { return }
+            
+            Observable.just(Reactor.Action.refreshBegin).bind(to: reactor.action).disposed(by: this.disposeBag)
+        })
+        
+        let footer = MJRefreshAutoNormalFooter(refreshingBlock: {[weak self] in
+            guard let this = self else { return }
+            Observable.just(Reactor.Action.loadMoreBegin).delaySubscription(1, scheduler: MainScheduler.instance).bind(to: reactor.action).disposed(by: this.disposeBag)
+        })
+        
+        footer.setTitle("", for: .idle)
+        self.tableView.mj_footer = footer
+        
+        self.filterButton.rx.tap.flatMapLatest {[weak self] (_) -> Observable<Reactor.Action> in
+            guard let this = self else { return .empty() }
+            return Observable<Reactor.Action>.create { (observer) -> Disposable in
+                this.showActionSheet(title: "选择消息类型", message: nil, buttonTitles: ["门锁消息", "资产消息"], highlightedButtonIndex: 0) { (index) in
+                    observer.onNext(Reactor.Action.changeMessageType(index))
+                    observer.onCompleted()
+                }
+                return Disposables.create()
+            }
+        }.bind(to: reactor.action).disposed(by: disposeBag)
+        
+        reactor.state.map { $0.IsNomoreData }.delay(1, scheduler: MainScheduler.instance).subscribe(onNext: {[weak self] (noMore) in
+            if noMore {
+                self?.tableView.mj_footer?.endRefreshingWithNoMoreData()
+            }
+        }).disposed(by: disposeBag)
+        
+        reactor.state.map { $0.pageIndex }.distinctUntilChanged().subscribe(onNext: { (index) in
+            print("当前页码\(index)")
+        }).disposed(by: disposeBag)
+        
+        reactor.state.map { $0.requestFinish }.subscribe(onNext: {[weak self] (_) in
+            self?.tableView.mj_header?.endRefreshing()
+            self?.tableView.mj_footer?.endRefreshing()
+        }).disposed(by: disposeBag)
+        
+        reactor.state.map { $0.messageList }.subscribe(onNext: {[weak self] (list) in
+            self?.dataSource = list
+            self?.tableView.reloadData()
+            }, onError: { (error) in
+                PKHUD.sharedHUD.rx.showError(error)
+        }).disposed(by: disposeBag)
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return dataSource.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "MessageCenterCell", for: indexPath) as! MessageCenterCell
+        
+        cell.bind(dataSource[indexPath.row])
+        return cell
     }
 }
--- LightSmartLock/Controllers/Lock/ViewModel/MessageCenterViewModel.swift
+++ LightSmartLock/Controllers/Lock/ViewModel/MessageCenterViewModel.swift
@@ -7,9 +7,111 @@
 //
 
 import Foundation
+import ReactorKit
+import RxSwift
 
 
-final class MessageCenterViewModel {
+final class MessageCenterReactor: Reactor {
     
+    enum Action {
+        case refreshBegin
+        case loadMoreBegin
+        case changeMessageType(Int)
+    }
     
+    struct State {
+        var pageIndex: Int
+        var IsNomoreData: Bool
+        var requestFinish: Bool
+        var messageType: Int
+        var messageList: [CenterMessageModel]
+    }
+    
+    enum Mutation {
+        case setRefreshPageIndex(Int)
+        case setLoadMorePageIndex(Int)
+        case setRequestFinished(Bool)
+        case setNomoreData(Bool)
+        case setMessageType(Int)
+        case setLoadMoreResult([CenterMessageModel])
+        case setRefreshResult([CenterMessageModel])
+    }
+    
+    let initialState: State
+    
+    init() {
+        self.initialState = State(pageIndex: 1, IsNomoreData: false, requestFinish: true, messageType: 1, messageList: [])
+    }
+    
+    func mutate(action: Action) -> Observable<Mutation> {
+        
+        switch action {
+        case .refreshBegin:
+            if !self.currentState.requestFinish {
+                return .empty()
+            }
+            
+            let request = BusinessAPI.requestMapJSONArray(.getLockNotice(noticeType: [-1], noticeLevel: [-1], pageIndex: 1, pageSize: 15), classType: CenterMessageModel.self, useCache: true).map { $0.compactMap{ $0 } }.share(replay: 1, scope: .forever)
+            
+            return Observable.concat([
+                .just(.setRefreshPageIndex(1)),
+                .just(.setNomoreData(false)),
+                request.map(Mutation.setRefreshResult),
+                request.map { _ in Mutation.setRequestFinished(true) }
+            ])
+            
+        case .loadMoreBegin:
+            if !self.currentState.requestFinish {
+                return .empty()
+            }
+            
+            let request = BusinessAPI.requestMapJSONArray(.getLockNotice(noticeType: [-1], noticeLevel: [-1], pageIndex: self.currentState.pageIndex + 1, pageSize: 15), classType: CenterMessageModel.self, useCache: true).map { $0.compactMap{ $0 } }.share(replay: 1, scope: .forever)
+            
+            return Observable.concat([
+                .just(.setLoadMorePageIndex(1)),
+                request.map { _ in Mutation.setRequestFinished(true) },
+                request.map({ (list) -> Mutation in
+                    if list.count == 0 {
+                        return Mutation.setNomoreData(true)
+                    } else {
+                        return Mutation.setNomoreData(false)
+                    }
+                }),
+                request.map(Mutation.setLoadMoreResult)
+            ])
+            
+        case .changeMessageType:
+            return .empty()
+            
+        }
+    }
+    
+    func reduce(state: State, mutation: Mutation) -> State {
+        var state = state
+        
+        switch mutation {
+        case let .setRequestFinished(finished):
+            state.requestFinish = finished
+            
+        case let .setNomoreData(noMore):
+            state.IsNomoreData = noMore
+            
+        case let .setLoadMorePageIndex(index):
+            state.pageIndex += index
+            print(state.pageIndex)
+        case let .setRefreshPageIndex(index):
+            state.pageIndex = index
+            
+        case let .setRefreshResult(list):
+            state.messageList = list
+            
+        case let .setLoadMoreResult(list):
+            state.messageList += list
+            
+        case let .setMessageType(type):
+            state.messageType = type
+        }
+        
+        return state
+    }
 }
