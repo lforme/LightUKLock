c0de3e8f863ca795148e231fcb4e06d0c1c09421
木瓜 <lforme@163.com>
2019-12-02 21:39:09 +0800

    音量设置

---
+++ LightSmartLock/Controllers/Lock/SoundSettingController.swift
@@ -0,0 +1,37 @@
+//
+//  SoundSettingController.swift
+//  LightSmartLock
+//
+//  Created by mugua on 2019/12/2.
+//  Copyright © 2019 mugua. All rights reserved.
+//
+
+import UIKit
+import RxSwift
+import RxCocoa
+import PKHUD
+
+class SoundSettingController: UIViewController {
+    
+    @IBOutlet weak var sliderView: UISlider!
+    
+    var vm: LockSettingViewModel!
+    
+    deinit {
+        print("\(self) deinit")
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        title = "门锁音量"
+        bind()
+    }
+    
+    func bind() {
+        
+        sliderView.rx.value.throttle(1.5, scheduler: MainScheduler.instance).map { Int($0) }.subscribe(onNext: {[weak self] (volume) in
+            self?.vm.setVolume(volume)
+        }).disposed(by: rx.disposeBag)
+    }
+}
