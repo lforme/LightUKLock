73bd439992d956f64081f848d9dd7837df3ad14a
木瓜 <lforme@163.com>
2019-12-06 17:33:52 +0800

    成员列表

---
+++ LightSmartLock/Models/UserMembersModels.swift
@@ -0,0 +1,34 @@
+//
+//  UserMembersModels.swift
+//  IntelligentUOKO
+//
+//  Created by mugua on 2018/11/5.
+//  Copyright © 2018 mugua. All rights reserved.
+//
+
+import Foundation
+import HandyJSON
+
+struct UserMemberListModel: HandyJSON {
+    
+    var customerID: String!
+    var accountID: String!
+    var sceneID: String!
+    var customerNickName: String?
+    var phone: String?
+    var keyID: String!
+    var headPic: String?
+    
+    var codeModel: Bool = false
+    var fingerprintModel: Bool = false
+    var cardModel: Bool = false
+    var bluetoothModel: Bool = false
+    
+    var userCode: String?
+    var createDate: String!
+    
+    var Label: String?
+    var initialSecret: String?
+    
+    var relationType: RoleModel?
+}
