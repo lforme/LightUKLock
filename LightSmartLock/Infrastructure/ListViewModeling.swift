d9f2b6b3a8acdda7065da883911ffa7c799f852e
木瓜 <lforme@163.com>
2019-12-06 17:34:13 +0800

    add base list viewmodel

---
+++ LightSmartLock/Infrastructure/ListViewModeling.swift
@@ -0,0 +1,25 @@
+//
+//  ListViewModeling.swift
+//  LightSmartLock
+//
+//  Created by mugua on 2019/12/6.
+//  Copyright © 2019 mugua. All rights reserved.
+//
+
+import Foundation
+import RxSwift
+import RxCocoa
+
+
+protocol ListViewModeling {
+    
+    associatedtype Item
+    
+    var refreshStaus: Observable<UKRefreshStatus> { get }
+    var list: Observable<[Item]> { get }
+    var pageIndex: Int { set get }
+    var disposeBag: DisposeBag { get }
+    
+    func refresh()
+    func loadMore()
+}
