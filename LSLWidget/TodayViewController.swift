72d9932f4e871e3b308a716e444310b582e8e4bd
木瓜 <lforme@163.com>
2020-01-03 16:43:05 +0800

    修改样式

--- LSLWidget/TodayViewController.swift
+++ LSLWidget/TodayViewController.swift
@@ -32,7 +32,7 @@ class TodayViewController: UIViewController, NCWidgetProviding {
     func setupUI() {
         tableView.tableFooterView = UIView()
         tableView.register(UINib(nibName: "WidgetUnlockRecordCell", bundle: nil), forCellReuseIdentifier: "WidgetUnlockRecordCell")
-        tableView.rowHeight = 76
+        tableView.rowHeight = 50
         tableView.separatorStyle = .none
     }
     
@@ -46,16 +46,7 @@ class TodayViewController: UIViewController, NCWidgetProviding {
             cell.nameLabel.text = item.customerNickName
             cell.timeLabel.text = item.UnlockTime
             cell.unlockWayLabel.text = item.KeyType.description
-            switch ip.section {
-            case 0:
-                cell.bgView.backgroundColor = #colorLiteral(red: 0.9534692168, green: 0.4842274189, blue: 0.1130582169, alpha: 1)
-            case 1:
-                cell.bgView.backgroundColor = #colorLiteral(red: 0.009328175336, green: 0.5046331882, blue: 1, alpha: 1)
-            case 2:
-                cell.bgView.backgroundColor = #colorLiteral(red: 0.611283958, green: 0.1478919685, blue: 0.6898549199, alpha: 1)
-            default: break
-            }
-            
+        
             return cell
         })
         
@@ -79,7 +70,7 @@ class TodayViewController: UIViewController, NCWidgetProviding {
         case .compact:
             preferredContentSize = maxSize
         case .expanded:
-            preferredContentSize = CGSize(width: 0.0, height: 70 * CGFloat(5))
+            preferredContentSize = CGSize(width: 0.0, height: 50 * CGFloat(5) + 10)
         @unknown default:
             fatalError()
         }
--- LSLWidget/View/WidgetUnlockRecordCell.swift
+++ LSLWidget/View/WidgetUnlockRecordCell.swift
@@ -13,12 +13,8 @@ class WidgetUnlockRecordCell: UITableViewCell {
     @IBOutlet weak var nameLabel: UILabel!
     @IBOutlet weak var unlockWayLabel: UILabel!
     @IBOutlet weak var timeLabel: UILabel!
-    @IBOutlet weak var bgView: UIView!
     
     override func awakeFromNib() {
         super.awakeFromNib()
-        
-        bgView.clipsToBounds = true
-        bgView.layer.cornerRadius = 7
     }
 }
