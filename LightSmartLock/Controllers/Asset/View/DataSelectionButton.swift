840579d7db96225f7ee84ffc015fa9d3f87bd827
changjun <changjun@uoko.com>
2020-04-27 17:42:37 +0800

    选择按钮1

--- LightSmartLock/Controllers/Asset/View/DataSelectionButton.swift
+++ LightSmartLock/Controllers/Asset/View/DataSelectionButton.swift
@@ -13,32 +13,33 @@ import RxOptional
 
 class DataSelectionButton: UIButton {
     
-    var items: [String] = ["1", "2", "3"]
-    var selectedIndex: Int?
-    var title: String? = "请选择"
+    var items: [[String]] = [["1", "2", "3"], ["年", "月", "日"]]
+    var result: [PickerResult]?  {
+           didSet {
+            guard let result = result, !result.isEmpty else {
+                return
+            }
+            let str = result.compactMap({ (r) -> String? in
+                return r.value
+            }).reduce("", { (next, acc) -> String in
+                return next + acc
+            })
+            self.setTitle(str, for: .normal)
+           }
+       }
+       
+    var title: String = "请选择"
     
     override func awakeFromNib() {
         super.awakeFromNib()
         setTitle(title, for: .normal)
         rx.tap
             .asObservable()
-            .flatMapFirst { [unowned self]_ -> Observable<Int> in
-                guard !self.items.isEmpty else {
-                    return .empty()
-                }
-                var initialItem: (Int, Int)?
-                if let selectedIndex = self.selectedIndex {
-                    initialItem = (0, selectedIndex)
-                }
-                return DataPickerViewController.rx
-                    .show(values: [self.items], initialSelection: initialItem)
-                    .map { $0?.row }
-                    .asObservable()
-                    .filterNil()
+            .flatMapFirst { [unowned self]_ -> Observable<[PickerResult]> in
+               return DataPickerController.rx.present(with: self.title, items: self.items)
         }
-        .subscribe(onNext: { [weak self](index) in
-            self?.selectedIndex = index
-            self?.setTitle(self?.items[index], for: .normal)
+        .subscribe(onNext: { [weak self](result) in
+            self?.result = result
         })
             .disposed(by: rx.disposeBag)
         
--- LightSmartLock/Controllers/Asset/View/DateSelectionButton.swift
+++ LightSmartLock/Controllers/Asset/View/DateSelectionButton.swift
@@ -13,10 +13,11 @@ import RxCocoa
 class DateSelectionButton: UIButton {
     
     var title: String = "请选择日期"
-    var selectedDate: Date? {
+    var maxDate: Date?
+    var minDate: Date?
+    var selectedDateStr: String? {
         didSet {
-            let dateStr = selectedDate?.toFormat("yyyy-MM-dd")
-            self.setTitle(dateStr, for: .normal)
+            self.setTitle(selectedDateStr, for: .normal)
         }
     }
     
@@ -36,13 +37,11 @@ class DateSelectionButton: UIButton {
 //            .disposed(by: rx.disposeBag)
         rx.tap
             .asObservable()
-            .flatMapFirst { [unowned self]_ -> Observable<Date> in
-                return DatePickerViewController.rx.show(date: self.selectedDate, min: nil, max: nil, mode: .date)
-                    .asObservable()
-                    .filterNil()
+            .flatMapFirst { [unowned self]_ -> Observable<String> in
+                return DatePickerController.rx.present(with: "yyyy-MM-dd", mode: .date, maxDate: self.maxDate, miniDate: self.minDate)
         }
-        .subscribe(onNext: { [weak self](date) in
-            self?.selectedDate = date
+        .subscribe(onNext: { [weak self]selectedDateStr in
+            self?.selectedDateStr = selectedDateStr
         })
             .disposed(by: rx.disposeBag)
     }
