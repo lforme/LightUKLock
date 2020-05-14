b1a6c3686296ab70888aae15b731af1f7e29902d
changjun <changjun@uoko.com>
2020-05-14 10:43:11 +0800

    字符串插值

--- LightSmartLock/Extensions/String+Extension.swift
+++ LightSmartLock/Extensions/String+Extension.swift
@@ -85,3 +85,33 @@ extension Double {
         return "¥" + twoPoint
     }
 }
+
+extension String.StringInterpolation {
+    /// Prints `Optional` values by only interpolating it if the value is set. `nil` is used as a fallback value to provide a clear output.
+    mutating func appendInterpolation<T: CustomStringConvertible>(_ value: T?) {
+        appendInterpolation(value ?? "nil" as CustomStringConvertible)
+        
+    }
+    
+    mutating func appendInterpolation(json JSONData: Data) {
+        guard
+            let JSONObject = try? JSONSerialization.jsonObject(with: JSONData, options: []),
+            let jsonData = try? JSONSerialization.data(withJSONObject: JSONObject, options: .prettyPrinted) else {
+            appendInterpolation("Invalid JSON data")
+            return
+        }
+        appendInterpolation("\n\(String(decoding: jsonData, as: UTF8.self))")
+    }
+    
+    mutating func appendInterpolation(_ request: URLRequest) {
+        appendInterpolation("\(request.url) | \(request.httpMethod) | Headers: \(request.allHTTPHeaderFields)")
+    }
+    
+    mutating func appendInterpolation(HTMLLink link: String) {
+        guard let url = URL(string: link) else {
+            assertionFailure("An invalid URL has been passed.")
+            return
+        }
+        appendInterpolation("<a href=\"\(url.absoluteString)\">\(url.absoluteString)</a>")
+    }
+}
