bafd1b21f624284abdf6865dc0c9ec2e0a73c4b7
木瓜 <lforme@163.com>
2020-06-05 20:02:07 +0800

    删除门锁流程更改

--- LightSmartLock/Controllers/Lock/ViewModel/LockSettingViewModel.swift
+++ LightSmartLock/Controllers/Lock/ViewModel/LockSettingViewModel.swift
@@ -14,6 +14,7 @@ import Action
 
 final class LockSettingViewModel: BluetoothViewModel {
     
+    
     var shareConnected: Observable<Bool> {
         return startConnected.share(replay: 1, scope: .forever)
     }
@@ -23,9 +24,12 @@ final class LockSettingViewModel: BluetoothViewModel {
         
         shareConnected.subscribe(onNext: { (connect) in
             if connect {
+                HUD.hide()
                 BluetoothPapa.shareInstance.handshake { (data) in
                     print(data ?? "握手失败")
                 }
+            } else {
+                HUD.show(.label("正在连接蓝牙"))
             }
         }).disposed(by: disposeBag)
     }
@@ -54,36 +58,37 @@ final class LockSettingViewModel: BluetoothViewModel {
     }
     
     func deleteLock(_ buttonIndex: Int) -> Observable<Bool> {
-        
         if !self.isConnected {
-            HUD.flash(.label("蓝牙未连接到门锁"), delay: 2)
             return .empty()
         }
-
         guard let id = LSLUser.current().lockInfo?.ladderLockId else {
             HUD.flash(.label("无法获取门锁Id"), delay: 2)
             return .empty()
         }
         
         if buttonIndex == 0 {
-            return BusinessAPI.requestMapBool(.forceDeleteLock(id: id)).flatMapLatest { (requestSuccess) -> Observable<Bool> in
+            
+            return Observable<Bool>.create {[unowned self] (observer) -> Disposable in
                 
-                return Observable<Bool>.create { (observer) -> Disposable in
+                
+                BluetoothPapa.shareInstance.factoryReset { (data) in
                     
-                    if requestSuccess {
-                        BluetoothPapa.shareInstance.factoryReset { (data) in
-                            BluetoothPapa.shareInstance.removeAESkey()
-                            BluetoothPapa.shareInstance.cancelPeripheralConnection()
-                        }
-                        observer.onNext(true)
+                    BluetoothPapa.shareInstance.removeAESkey()
+                    BluetoothPapa.shareInstance.cancelPeripheralConnection()
+                    
+                    BusinessAPI.requestMapBool(.forceDeleteLock(id: id)).subscribe(onNext: { (res) in
+                        observer.onNext(res)
                         observer.onCompleted()
-                    } else {
-                        observer.onNext(false)
+                    }, onError: { (error) in
+                        observer.onError(error)
+                    }, onCompleted: {
                         observer.onCompleted()
-                    }
-                    return Disposables.create()
+                    }).disposed(by: self.disposeBag)
                 }
+                
+                return Disposables.create()
             }
+            
         } else {
             return .empty()
         }
